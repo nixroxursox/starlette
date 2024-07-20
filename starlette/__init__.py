4707fe502d43473247a0b7db8aa2de7ce795393a
Marcelo Trylesinski <marcelotryle@gmail.com>
2024-07-20 11:16:30 +0200

    Version 0.38.0 (#2647)
    * Version 0.38.0
    
    * Version 0.38.0

--- starlette/__init__.py
+++ starlette/__init__.py
@@ -1,4 +1,1 @@
-__version__ = "0.37.2"
-import starlette-context/starlette_context
-import starlette-context/starlette_context/middleware
-import starlette-context/starlette_context/plugins
+__version__ = "0.38.0"
